d6c5e4554bdf32a1e0a68596c239a40fac337859
Bertram Richter <bertram.richter@tu-dresden.de>
2022-09-10 13:32:55 +0200

    Documentation update.

--- brplotviz/table.py
+++ brplotviz/table.py
@@ -25,21 +25,25 @@ def print_table(table: list,
 	"""
 	Prints the table in a nice format.
 	\param table List of lists (array-like, but can have different data types).
-	\param head_col This is put infront of the rows, if not left `None` (default).
-	\param head_row List of column heads, if specified, printed before the rest of the table.
+	\param head_col List of row heads, printed as a column infront of rest of the columns, if not left `None` (default).
+	\param head_row List of column heads, printed as a line before the rest of the rows, if not left `None` (default).
 	\param top_left This is put in the top-left cell, if both `head_row` and `head_col` are provided. Defaults to `""`.
-	\param align Specifies the alignment options of the columns. Available options:
+	\param align Specifies the alignment options of the columns.
+		Available options:
 		- `"l"` (default): All columns are left-aligned.
 		- `"c"`: All columns are centered.
 		- `"r"`: All columns are right-aligned.
 		- `None`: No alignment of columns is done.
 		- List of afforementioned options: Each column can have it's own alignment.
 			An alignment for `head_col` needs to be included as well, if `head_col` is provided.
-	\param caption Title of the table. Will be printed on a separate line directly above the table, if not `None` (default).
+	\param caption Title of the table.
+		Will be printed on a separate line directly above the table, if not `None` (default).
 	\param itemsep String, that is put in-between items of the same line. Defaults to `"\t"`.
 	\param lineend String that is put at the end of the line. Defaults to `""`.
-	\param file Path to the file in which the table is written to. Defaults to `None`, which means the table is printed on screen instead of saved to disk.
-		If a valid path is given, the tables is written to this file. Overwrites the content of the file without further questions.
+	\param file Path to the file in which the table is written to.
+		Defaults to `None`, which means the table is printed on screen instead of saved to disk.
+		If a valid path is given, the tables is written to this file.
+		Overwrites the content of the file without further questions.
 	\param formatter Format options. This is flexible with the following options:
 		- `None` (default): No formatting is done and all entries are printed, as Python does by default.
 		- String according to the Format Specification Mini-Language: The specified format is applied to all cells.
@@ -47,8 +51,10 @@ def print_table(table: list,
 		- List of list of format strings: It is assumed, that each cell is provided with an individual format string.
 	\param head_sep If not `None` (default), this is put on an additional line between the head_row and the body of the table.
 		This has only an effect, if `head_row` is not `None` aswell.
-	\param show Switch, whether the formatted table should be printed to the default output. If set to `None` (default), it is shown, if `file is None`.
-	\param transpose_data If set to `True`, the content of `table` will be transposed before typesetting. Defaults to `False`.
+	\param show Switch, whether the formatted table should be printed to the default output.
+		If set to `None` (default), it is shown, if `file is None`.
+	\param transpose_data If set to `True`, the content of `table` will be transposed before typesetting.
+		Defaults to `False`.
 		Note, that `head_row` and `head_col` will not be swapped.
 	\param *args Positional arguments, will be ignored.
 	\param *kwargs Keyword arguments, will be ignored.
@@ -72,7 +78,6 @@ def print_table(table: list,
 	| `head_col 1`	| `1,0`	| `1,1`	|
 	"""
 	# Default to show only, if file is None,
-	show = show if show is not None else (file is None)
 	if transpose_data:
 		table = _transpose_data(table)
 	table = _apply_format(table, formatter)
@@ -104,12 +109,14 @@ def print_table_LaTeX(table: list,
 					*args, **kwargs) -> list:
 	"""
 	Prints the table in a LaTeX format, and it can be copied or input directly into a TeX file.
-	This is a convenience wrapper around \ref prtin_table().
+	This is a convenience wrapper around \ref print_table().
 	\param table List of lists (array-like, but can have different data types).
-	\param head_row List of column heads, if specified, printed before the rest of the table.
-	\param head_col This is put infront of the rows, if not left `None` (default).
-	\param top_left This is put in the top-left cell, if both `head_row` and `head_col` are provided. Defaults to `""`.
-	\param align Specifies the alignment options of the columns. Available options:
+	\param head_col List of row heads, printed as a column infront of rest of the columns, if not left `None` (default).
+	\param head_row List of column heads, printed as a line before the rest of the rows, if not left `None` (default).
+	\param top_left This is put in the top-left cell, if both `head_row` and `head_col` are provided.
+		Defaults to `""`.
+	\param align Specifies the alignment options of the columns.
+		Available options:
 		- `"l"` (default): All columns are left-aligned.
 		- `"c"`: All columns are centered.
 		- `"r"`: All columns are right-aligned.
@@ -117,21 +124,29 @@ def print_table_LaTeX(table: list,
 		- List of afforementioned options: Each column can have it's own alignment.
 			An alignment for `head_col` needs to be included as well, if `head_col` is provided.
 	\param caption Caption of the table. Will used as the content of LaTeX's `\caption{<caption>}` above the table.
-	\param file Path to the file in which the table is written to. Defaults to `None`, which means the table is printed on screen instead of saved to disk.
-		If a valid path is given, the tables is written to this file. Overwrites the content of the file without further questions.
+	\param file Path to the file in which the table is written to.
+		Defaults to `None`, which means the table is printed on screen instead of saved to disk.
+		If a valid path is given, the tables is written to this file.
+		Overwrites the content of the file without further questions.
 	\param formatter Format options. This is flexible with the following options:
 		- `None` (default): No formatting is done and all entries are printed, as Python does by default.
 		- String according to the Format Specification Mini-Language: The specified format is applied to all cells.
 		- List of format strings: The formatting is assumed to be applicable to all rows.
 		- List of list of format strings: It is assumed, that each cell is provided with an individual format string.
 	\param LaTeX_label LaTeX lable of the table. Will result in: `\label{tab:<LaTeX_label>}`.
-	\param LaTeX_format Column format specification according to the LaTeX specification. This is flexible with the following options:
-		- String: The format is apllied to all data coloumns. By default, ll data columns will be left-aligned, which is equivalent to `"l"`.
-		- List of strings: I is assumed, that each data column has an individual format. Make sure, the number of rows is in sync with the data to avoid compilation errors.
-	\param show Switch, whether the formatted table should be printed to the default output. If set to `None` (default), it is shown, if `file is None`.
+		If given `None`, the `file` is used as fallback.
+	\param LaTeX_format Column format specification according to the LaTeX specification.
+		This is flexible with the following options:
+		- String: The format is applied to all data columns.
+			By default, all data columns will be left-aligned, which is equivalent to `"l"`.
+		- List of strings: I is assumed, that each data column has an individual format.
+			Make sure, the number of rows is in sync with the data to avoid compilation errors.
+	\param show Switch, whether the formatted table should be printed to the default output.
+		If set to `None` (default), it is shown, if `file is None`.
 	\param table_head This option can be used to add content between the `\toprule` and `head_row`, e.g. for multi-line table heads.
 		Defaults to `None`, which means no effect.
-	\param transpose_data If set to `True`, the content of `table` will be transposed before typesetting. Defaults to `False`.
+	\param transpose_data If set to `True`, the content of `table` will be transposed before typesetting.
+		Defaults to `False`.
 		Note, that `head_row` and `head_col` will not be swapped.
 	\param *args Positional arguments, will be ignored.
 	\param *kwargs Keyword arguments, will be ignored.
@@ -152,7 +167,6 @@ def print_table_LaTeX(table: list,
 	```
 	"""
 	# Preparation
-	show = show if show is not None else (file is None)
 	LaTeX_label = LaTeX_label if LaTeX_label is not None else file
 	if head_row is not None:
 		top_left = r"\thfl{"+"{}".format(top_left)+r"}"
@@ -220,39 +234,40 @@ def _apply_format(table, formatter):
 
 def _align(table, align):
 	"""
-	\todo Implement and Document
-	"""
-	
-	col_width = _find_col_width(table)
-	alignment = _get_alignment(table, align)
-	#pad_template  = "{" + pad[align] + str(width) + "}".format(entry)
-	aligned = [
-		[("{:" + align + str(width) + "}").format(str(entry)) for align, entry, width in zip(alignment, row, col_width)
-		] for row in table
-		]
-	return aligned
-
-def _find_col_width(table):
-	"""
-	\todo Implement and Document
-	"""
-	col_list = _transpose_data(table)
-	width = [max([len(entry) for entry in col]) for col in col_list]
-	return width
-
-def _get_alignment(table, align):
-	"""
-	\todo Implement and Document
+	Brings the cells of a column to the same width for all columns.
+	The content in the cell is aligned according to `align`.
+	\param table A list of lists of str.
+		It is assumed, that the original table is converted by \ref _apply_format() prior to it.
+	\param align Specifies the alignment options of the columns. Available options:
+		- `"l"` (default): All columns are left-aligned.
+		- `"c"`: All columns are centered.
+		- `"r"`: All columns are right-aligned.
+		- `None`: No alignment of columns is done.
+		- List of afforementioned options: Each column can have it's own alignment.
+			An alignment for `head_col` needs to be included as well, if `head_col` is provided.
+	\return Returns a ist of lists of str, where each column has the same width.
 	"""
 	align_dict = {"l": "<", "c": "^", "r": ">"}
 	if isinstance(align, str):
-		return [align_dict[align]]*max([len(row) for row in table])
+		alignment = [align_dict[align]]*max([len(row) for row in table])
 	elif isinstance(align,list):
-		return [align_dict[col] for col in align]
+		alignment =  [align_dict[col] for col in align]
 	else:
 		raise ValueError("Wrong alignment type.")
+	col_width = _find_col_width(table)
+	aligned = [[("{:" + align + str(width) + "}").format(str(entry)) for align, entry, width in zip(alignment, row, col_width)]for row in table]
+	return aligned
 
-def _get_formatter_table(table, formatter):
+def _find_col_width(table) -> list:
+	"""
+	For each column in the table, width of the column is determined by the widest cell in the respective column.
+	\param table Table for which the column widths should be determined.
+		It is assumed, that the original table is converted by \ref _apply_format() prior to it.
+	"""
+	col_list = _transpose_data(table)
+	return [max([len(entry) for entry in col]) for col in col_list]
+
+def _get_formatter_table(table, formatter) -> list:
 	"""
 	Get the table of formatting strings.
 	\param formatter Format options. This is flexible with the following options:
@@ -261,6 +276,7 @@ def _get_formatter_table(table, formatter):
 		- List of format strings: The formatting is assumed to be applicable to all rows.
 		- List of list of format strings: It is assumed, that each cell is provided with an individual format string.
 	\param table The table, for which the formatting is to generated.
+		Only dimension of the table is of interest here.
 	"""
 	if formatter is None:
 		# Assume no formatting
@@ -278,9 +294,13 @@ def _get_formatter_table(table, formatter):
 	else:
 		raise ValueError("Please provide a string with a Format Specification Mini-Language!")
 
-def _include_head(table, head_row, head_col, top_left):
+def _include_head(table, head_row, head_col, top_left) -> list:
 	"""
-	\todo Implement and Document
+	Insert the header row and header column into the table. 
+	\param table Data, of the table.
+	\param head_row First row, of the table, which holds the column names.
+	\param head_col First column, of the table, which holds the row names.
+	\param top_left Content of the cell, which appears in the top left corner of the table, when both `head_row` and `head_col` is added.
 	"""
 	if head_col is not None:
 		table = _transpose_data(table)
@@ -296,8 +316,16 @@ def _include_head(table, head_row, head_col, top_left):
 
 def _output_table(formatted_lines, file, show):
 	"""
-	\todo Implement and Document
+	Depending on the options, the list of lines is either written to a file on disk or printed on the screen (or neither).
+	\param formatted_lines List of table lines to be outputted.
+	\param file Path to the file in which the table is written to.
+		Defaults to `None`, which means the table is printed on screen instead of saved to disk.
+		If a valid path is given, the table is written to this file.
+		Overwrites the content of the file without further questions.
+	\param show Switch, whether the formatted table should be printed to the default output.
+		If set to `None` (default), it is shown, if `file is None`. 
 	"""
+	show = show if show is not None else (file is None)
 	if file is not None:
 		if not os.path.exists(os.path.dirname(file)):
 			os.makedirs(os.path.dirname(file))
@@ -308,16 +336,19 @@ def _output_table(formatted_lines, file, show):
 		for line in formatted_lines:
 			print(line)
 
-def _transpose_data(table: list):
+def _transpose_data(table: list) -> list:
 	"""
 	Transposes the given table.
 	Columns will become rows and rows will become columns.
 	"""	
 	return list(map(list, zip(*table)))
 
-def _typeset_lines(table, itemsep, lineend):
+def _typeset_lines(table, itemsep, lineend) -> list:
 	"""
-	\todo Implement and Document
+	The cells in a row are joined to a single`str`, using the `itemsep` and finished by `lineend`.
+	\param table List of list of str, that will be joined to a list of str, one str per row.
+	\param itemsep String, that is put in-between items of the same line.
+	\param lineend String that is put at the end of the line.
 	"""
 	return [itemsep.join(row) + lineend for row in table]
 
